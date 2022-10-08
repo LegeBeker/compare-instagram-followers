9dd46d99034e1f29e9de48c1e79ec4ecaf2dd81b
LegeBeker <volkanwelp@gmail.com>
2022-10-08 13:32:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// if submit is pressed
+if (document.getElementById("submit").onclick) {
+    // get the value of the input
+    var input = document.getElementById("input").value;
+    // if the input is not empty
+    if (input != "") {
+    }
+    // retreive instagram followers
+    
+}
