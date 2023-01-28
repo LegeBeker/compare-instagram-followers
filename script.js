101a40423f05d128efe4d7ee0ed7f156b4a7ea8b
LegeBeker <volkanwelp@gmail.com>
2023-01-28 21:14:21 +0100

    ✨ Finish get followers

--- script.js
+++ script.js
@@ -2,46 +2,59 @@ let followers = [];
 let following = [];
 let followList = null;
 
-$("a[href*='/followers/']").click();
+(async function () {
+    await $("a[href*='/followers/']").click();
 
-getFollowList();
+    await getFollowersList();
 
-scrollToBottom(followList);
+    await scrollToBottom(followList);
 
-getFollowers();
+    await getFollowers();
 
-console.log(followers);
+    console.log(followers);
 
-// $("a[href*='/following/']").click();
+    // await $("a[href*='/following/']").click();
+})();
 
-
-function scrollToBottom(div) {
-    if (div.scrollTop + div.clientHeight < div.scrollHeight) {
-        div.scrollTop = div.scrollHeight;
-        setTimeout(scrollToBottom, 500);
-    }
-}
-
-function getFollowList() {
-    try {
-        followList = $("h1 div").parentElement.parentElement.parentElement.parentElement.parentElement.children[1];
-        if (followList === null) {
-            setTimeout(getFollowList, 500);
+async function getFollowersList() {
+    return new Promise((resolve) => {
+        try {
+            console.log("Getting follow list");
+            followList = document.querySelector("h1 div").parentElement.parentElement.parentElement.parentElement.parentElement.children[1];
+            console.log(followList);
+            if (followList === null) {
+                setTimeout(() => getFollowersList().then(resolve), 500);
+            } else {
+                resolve();
+            }
+        } catch (e) {
+            console.log("Failed to get follow list");
+            setTimeout(() => getFollowersList().then(resolve), 500);
         }
-    } catch (e) {
-        setTimeout(getFollowList, 500);
-    }
+    });
 }
 
-function getFollowers() {
-    if (followList.scrollTop + followList.clientHeight < followList.scrollHeight) {
+async function scrollToBottom(div) {
+    return new Promise((resolve) => {
+        console.log("Scrolling to bottom");
+        if (div.scrollTop + div.clientHeight < div.scrollHeight) {
+            div.scrollTop = div.scrollHeight;
+            setTimeout(() => scrollToBottom(div).then(resolve), 500);
+        } else {
+            resolve();
+        }
+    });
+}
+
+async function getFollowers() {
+    return new Promise((resolve) => {
+        console.log("Getting followers");
         var buttons = document.getElementsByTagName("button");
         for (var i = 0; i < buttons.length; i++) {
             if (buttons[i].innerHTML === "Remove") {
                 followers.push(buttons[i].parentElement.parentElement.children[1].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML);
             }
         }
-    } else {
-        setTimeout(getFollowers, 500);
-    }
+        resolve();
+    });
 }
