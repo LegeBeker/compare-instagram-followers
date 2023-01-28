918ae02590b85f59d13ca49c75ebd7ca22282687
LegeBeker <volkanwelp@gmail.com>
2023-01-29 00:07:35 +0100

    ✨ Functionality for other profiles

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+let TrueFollowersCount = 0;
+let TrueFollowingCount = 0;
 let followers = [];
 let following = [];
 let followList = null;
 
 (async function () {
+    document.documentElement.style.setProperty('--ig-link', 'darkblue');
+
     var overlay = document.createElement("div");
     overlay.style.position = "fixed";
+    overlay.style.overflow = "auto";
     overlay.style.top = "0";
     overlay.style.left = "0";
     overlay.style.width = "100%";
@@ -23,12 +28,17 @@ let followList = null;
     progressDiv.style.display = "flex";
     progressDiv.style.justifyContent = "center";
     progressDiv.style.alignItems = "center";
+    progressDiv.style.flexDirection = "column";
     progressDiv.classList.add("progressDiv");
 
     overlay.appendChild(progressDiv);
     document.body.appendChild(overlay);
 
+    document.querySelector(".progressDiv").innerHTML += "<progress></progress><br>";
+
     await closeAll()
+
+    await getCounts();
 
     await getFollowers();
     await getFollowing();
@@ -60,8 +70,21 @@ let followList = null;
 
     document.querySelector(".progressDiv").remove();
 
+    if (TrueFollowersCount !== followers.length || TrueFollowingCount !== following.length) {
+        var warning = document.createElement("div");
+        warning.style.color = "red";
+        warning.style.fontSize = "1.5em";
+        warning.style.fontWeight = "bold";
+        warning.style.margin = "20px";
+        warning.style.textAlign = "center";
+        warning.style.width = "100%";
+        warning.innerHTML = "Warning: not all followers/following were loaded. Results might be inaccurate. Please try again.";
+        document.querySelector(".overlay").appendChild(warning);
+    }
+
     var div = document.createElement("div");
-    div.style.width = "50%";
+    div.style.float = "left";
+    div.style.width = "30%";
 
     var h1 = document.createElement("h1");
     h1.innerHTML = "Not following back";
@@ -73,16 +96,18 @@ let followList = null;
     var ul = document.createElement("ul");
     for (var i = 0; i < notFollowingBack.length; i++) {
         var li = document.createElement("li");
-        li.innerHTML = notFollowingBack[i];
+        li.innerHTML = "<a href='https://www.instagram.com/" + notFollowingBack[i] + "/' target='_blank'>@" + notFollowingBack[i] + "</a>";
         ul.appendChild(li);
     }
-    ul.padding = "40px";
+    ul.style.listStyle = "disc";
+    ul.style.marginLeft = "40px";
     div.appendChild(ul);
 
     document.querySelector(".overlay").appendChild(div);
 
     var div = document.createElement("div");
-    div.style.width = "50%";
+    div.style.float = "left";
+    div.style.width = "30%";
 
     var h1 = document.createElement("h1");
     h1.innerHTML = "Not following";
@@ -94,20 +119,38 @@ let followList = null;
     var ul = document.createElement("ul");
     for (var i = 0; i < notFollowing.length; i++) {
         var li = document.createElement("li");
-        li.innerHTML = notFollowing[i];
+        li.innerHTML = "<a href='https://www.instagram.com/" + notFollowing[i] + "/' target='_blank'>@" + notFollowing[i] + "</a>";
         ul.appendChild(li);
     }
-    ul.padding = "40px";
+    ul.style.listStyle = "disc";
+    ul.style.marginLeft = "40px";
     div.appendChild(ul);
 
     document.querySelector(".overlay").appendChild(div);
 })();
 
+async function getCounts() {
+    return new Promise((resolve) => {
+        document.querySelector(".progressDiv").innerHTML += "Getting followers count<br>";
+        console.log("Getting followers count");
+        var followersCount = document.querySelector("a[href*='/followers/'] span span").innerHTML;
+        followersCount = followersCount.replace(/,/g, "");
+        followersCount = parseInt(followersCount);
+
+        document.querySelector(".progressDiv").innerHTML += "Getting following count<br>";
+        console.log("Getting following count");
+        var followingCount = document.querySelector("a[href*='/following/'] span span").innerHTML;
+        followingCount = followingCount.replace(/,/g, "");
+        followingCount = parseInt(followingCount);
+        resolve();
+    });
+}
+
 async function getFollowers() {
     document.querySelector("a[href*='/followers/']").click();
 
     await getFollowList(1);
-    await scrollToBottom(followList);
+    await scrollToBottom(followList.parentElement.parentElement);
     await getFollow("Followers");
     document.querySelector(".progressDiv").innerHTML += "Done getting followers<br><br>";
 }
@@ -116,7 +159,7 @@ async function getFollowing() {
     document.querySelector("a[href*='/following/']").click();
 
     await getFollowList(2);
-    await scrollToBottom(followList);
+    await scrollToBottom(followList.parentElement.parentElement);
     await getFollow("Following");
     document.querySelector(".progressDiv").innerHTML += "Done getting following<br><br>";
 }
@@ -127,8 +170,9 @@ async function getFollowList(child) {
             followList = null;
             document.querySelector(".progressDiv").innerHTML += "Getting list element<br>";
             console.log("Getting list element");
-            followList = document.querySelector("h1 div").parentElement.parentElement.parentElement.parentElement.parentElement.children[child];
-            if (followList === null || followList === undefined) {
+            followList = document.querySelector("h1 div").parentElement.parentElement.parentElement.parentElement.parentElement.children[child].firstChild.firstChild;
+            console.log(followList);
+            if (followList === null || followList === undefined || followList.children.length === 0) {
                 setTimeout(() => getFollowList(child).then(resolve), 500);
             } else {
                 document.querySelector(".progressDiv").innerHTML += "Got list element<br>";
@@ -159,19 +203,14 @@ async function getFollow(listType) {
     return new Promise((resolve) => {
         document.querySelector(".progressDiv").innerHTML += "Getting " + listType + "<br>";
         console.log("Getting followers");
-        var buttons = document.getElementsByTagName("button");
-        for (var i = 0; i < buttons.length; i++) {
-            if (listType === "Followers" && buttons[i].innerHTML === "Remove") {
-                var follower = buttons[i].parentElement.parentElement.children[1].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML;
-                if (follower.indexOf("<") !== -1) {
-                    follower = follower.substring(0, follower.indexOf("<"));
-                }
+        for (var i = 0; i < followList.children.length; i++) {
+            var follower = followList.children[i].children[1].firstChild.firstChild.firstChild.firstChild.firstChild.firstChild.firstChild.firstChild.innerHTML;
+            if (follower.indexOf("<") !== -1) {
+                follower = follower.substring(0, follower.indexOf("<"));
+            }
+            if (listType === "Followers") {
                 followers.push(follower);
-            } else if (listType === "Following" && buttons[i].children[0] && buttons[i].children[0].children[0] && buttons[i].children[0].children[0].innerHTML === "Following") {
-                var follower = buttons[i].parentElement.parentElement.children[1].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML;
-                if (follower.indexOf("<") !== -1) {
-                    follower = follower.substring(0, follower.indexOf("<"));
-                }
+            } else {
                 following.push(follower);
             }
         }
